49c0daf75914a1797425df9cc1e43e245ff5fc44
zedann <zedamohamed144@gmail.com>
2023-03-10 12:42:45 +0200

    date

---
+++ Date&Time/main.js
@@ -0,0 +1,12 @@
+// 1
+// let curDate = new Date();
+// let myBirthDay = new Date('jan 1, 2002');
+// let date = curDate - myBirthDay;
+// console.log(Math.trunc(date/1000) + " Seconds\n" ,Math.trunc(date/1000/60) + " Minutes\n",Math.trunc(date/1000/60/60) + " Hours\n",Math.trunc(date/1000/60/60/24) + " Days\n",Math.trunc(date/1000/60/60/24/30) + " Months\n",Math.trunc(date/1000/60/60/24/30/12) + " Years\n");
+//2
+// let date = new Date();
+// date.setTime(1000);
+// date.setFullYear(1980);
+// console.log(date);
+//3
+
