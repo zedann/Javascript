31c8b436b2386127580e79e144cf3c9963149510
zedann <zedamohamed144@gmail.com>
2023-09-16 08:54:37 +0300

    Object Start

---
+++ Objects/main.js
@@ -0,0 +1,9 @@
+const user = {
+    firstName : "zedan",
+    lastName : "mohamed",
+    age : 21,
+    job : "software eng",
+}
+// const users = [];
+// users.push(user);
+// console.log(users[0].firstName);
