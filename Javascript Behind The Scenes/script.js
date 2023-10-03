430f60ff5632368f948c75dd2fd21cb8b84ac322
zedann <zedamohamed144@gmail.com>
2023-10-03 21:36:20 +0300

    finish js under the hood

--- Javascript Behind The Scenes/script.js
+++ Javascript Behind The Scenes/script.js
@@ -27,3 +27,56 @@
 // };
 // calcAgeArrow(2003);
 
+// const jonas = {
+//   firstName: "jonas",
+//   year: 1991,
+//   calcAge: function () {
+//     //console.log(this);
+//     // console.log(2023 - this.year);
+//     const self = this;
+//     console.log(this);
+//     const isMillenial = function () {
+//       console.log(self.year >= 1981 && self.year <= 1996);
+//     };
+//     isMillenial(); //regular fun call this->undefined
+//   },
+//   greet: () => console.log("Hey " + this.firstName),
+// };
+// //var firstName = "zedan";
+// // jonas.greet();
+// // console.log(this);
+// jonas.calcAge();
+
+// function exp(...params) {
+//   console.log(params);
+//   // return a + b;
+// }
+// exp(2, 3, 4);
+// const arr = (...params) => {
+//   console.log(params);
+//   // return a + b;
+// };
+// arr(3, 1, 8);
+//primitives vs objects
+
+// const user = {
+//   userName: "username",
+//   lastName: "lastname",
+// };
+// console.log(user);
+//how to copy ref type(object)
+// const user1 = Object.assign({}, user); //only work on first level (primitives)
+// user1.userName = "zedan";
+// user1.lastName = "mohamed";
+// console.log(user1);
+
+// const users = [];
+// for (let i = 0; i < 100; i++) {
+//   const curUser = {
+//     firstName: `first${i}`,
+//     last: "last",
+//   };
+//   users.push(curUser);
+// }
+// console.log(user);
+// console.log(users);
