6d8b1eab9ae5f9ae2a56dffcabab4f697110e847
zedann <zedamohamed144@gmail.com>
2023-10-03 16:42:01 +0300

    start hoisting

--- Javascript Behind The Scenes/script.js
+++ Javascript Behind The Scenes/script.js
@@ -1,1 +1,29 @@
 "use strict";
+//1-scoping
+// const date = new Date().getFullYear();
+// function calcAge(birthYear) {
+//   const age = date - birthYear;
+// }
+// const firstName = "zedan";
+// console.log(calcAge(2003));
+
+//2-hoisting
+// fun(1);
+// const fun = function (x) {
+//   console.log(x);
+// };
+
+//3-this keyword
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2023 - birthYear);
+//   console.log(this);
+// };
+// const calcAgeArrow = (birthYear) => {
+//   console.log(2023 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(2003);
+
