524076105d552188471a156f7d97275d972c4c6d
Zedan <zedamohamed144@gmail.com>
2023-10-29 00:25:21 +0200

    End OOP

--- OOP/script.js
+++ OOP/script.js
@@ -148,46 +148,177 @@
 
 //challenge
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(this.speed);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
 
-Car.prototype.break = function () {
-  this.speed = Math.abs(this.speed - 5);
-  console.log(this.speed);
-};
+// Car.prototype.break = function () {
+//   this.speed = Math.abs(this.speed - 5);
+//   console.log(this.speed);
+// };
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
-  this.charge = charge;
-};
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
 
-EV.prototype = Object.create(Car.prototype);
-EV.prototype.constructor = EV;
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-  console.log(this.charge * 100 + '%');
-};
-EV.prototype.accelerate = function () {
-  //overriding
-  this.speed += 20;
-  this.charge -= 0.01;
-  console.log(
-    `${this.make} going at ${this.speed} km/h , with a charge of ${
-      this.charge * 100
-    }%`
-  );
-};
-// console.log(EV.prototype.accelerate);
+// EV.prototype = Object.create(Car.prototype);
+// EV.prototype.constructor = EV;
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+//   console.log(this.charge * 100 + '%');
+// };
+// EV.prototype.accelerate = function () {
+//   //overriding
+//   this.speed += 20;
+//   this.charge -= 0.01;
+//   console.log(
+//     `${this.make} going at ${this.speed} km/h , with a charge of ${
+//       this.charge * 100
+//     }%`
+//   );
+// };
+// // console.log(EV.prototype.accelerate);
 
-const tesla = new EV('Tesla', 120, 0.23);
-console.log(tesla);
-tesla.accelerate();
-tesla.break();
-tesla.chargeBattery(0.9);
+// const tesla = new EV('Tesla', 120, 0.23);
+// console.log(tesla);
+// tesla.accelerate();
+// tesla.break();
+// tesla.chargeBattery(0.9);
+
+//Inheritance ES6
+// class Person {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+
+//   calcAge() {
+//     return new Date().getFullYear() - this.birthYear;
+//   }
+// }
+
+// class Student extends Person {
+//   #course; //private
+//   constructor(firstName, birthYear, course) {
+//     super(firstName, birthYear);
+//     this.#course = course;
+//   }
+//   //overriding
+//   calcAge() {
+//     return new Date().getFullYear() - this.birthYear + 10;
+//   }
+//   introduce() {
+//     return `I'm ${this.fullName} and my age is ${this.calcAge()} and study ${
+//       this.course
+//     }`;
+//   }
+//   get course() {
+//     return this.#course;
+//   }
+//   set course(courseName) {
+//     this.#course = courseName;
+//   }
+// }
+
+// const zedan = new Student('zedan', 2003, 'CS');
+// console.log(zedan);
+// console.log(zedan.course);
+// console.log(zedan.calcAge());
+// console.log(zedan.introduce());
+
+//challenge #4
+
+//TODO:Revision On overriding using C#
+// class Car {
+//   make;
+//   speed;
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+//   accelerate() {
+//     this.speed += 10;
+//     return this.speed;
+//   }
+//   break() {
+//     this.speed -= 5;
+//     return this.speed;
+//   }
+// }
+// class EV extends Car {
+//   #charge;
+//   constructor(make, speed, charge) {
+//     super(make, speed);
+//     this.#charge = charge;
+//   }
+//   #approveCarModel() {
+//     return true;
+//   }
+
+//   validCar() {
+//     if (this.#approveCarModel()) {
+//       console.log('Yes Approved');
+//     }
+//   }
+//   static helper() {
+//     console.log('helper');
+//   }
+// }
+// const tesla = new EV('tesla', 120, 23);
+// console.log(tesla);
+// console.log(tesla);
+// EV.helper();
+
+//challenge #4
+class Car {
+  make;
+  speed;
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    return this;
+  }
+  break() {
+    this.speed -= 5;
+    return this;
+  }
+}
+class EV extends Car {
+  #charge = 0;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    return this;
+  }
+  break() {
+    this.#charge--;
+    return this;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+  getCharge() {
+    return this.#charge;
+  }
+}
+
+const rivian = new EV('Rivian', 120, 23);
+console.log(rivian);
+rivian.accelerate().accelerate().chargeBattery(90).break();
+console.log(rivian);
+
